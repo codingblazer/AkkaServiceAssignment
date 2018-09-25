943974bc90f21acfaa45d7bfcb740ef896509eff
Sachin Aggarwal <sasachin04@gmail.com>
2018-09-25 12:19:21 +0530

    Update push command with correct versioning

--- run_gocd.sh
+++ run_gocd.sh
@@ -1,4 +1,4 @@
 sbt clean test assembly
 docker build -t codingblazer/dockerimage:"$GO_REVISION" .
 echo $pass | docker login -u $user --password-stdin
-docker push codingblazer/dockerimage/"$GO_REVISION"
+docker push codingblazer/dockerimage:"$GO_REVISION"
